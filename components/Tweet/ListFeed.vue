e161adc7e04a127d9f05638521171348a7facb57
Gabriel Sena <stardusteight.d4cc@gmail.com>
2023-07-27 17:00:40 -0300

    getting homeTweets from server api

---
+++ components/Tweet/ListFeed.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+interface Props {
+  tweets: any[]
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <h1>ListFeed</h1>
+  <ul>
+    <li v-for="tweet in props.tweets">
+      {{ tweet }}
+    </li>
+  </ul>
+</template>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,35 @@
 <script setup lang="ts">
-const loading = ref(false)
 const { useAuthUser } = useAuth()
-const user = useAuthUser()
+const { getHomeTweets } = useTweets()
+const { twitterBorderColor } = useTailwindConfig()
+
+onBeforeMount(async () => {
+  loading.value = true
+  try {
+    const { tweets } = await getHomeTweets() as any
+    homeTweets.value = tweets
+  } catch (error) {
+    console.error(error)
+  } finally {
+    loading.value = false
+  }
+})
+
+const loading = ref(false)
+const user = useAuthUser() as unknown as User
+const homeTweets = ref([])
 </script>
 
 <template>
   <div>
     <MainSection title="Home" :loading="loading">
-      <!-- {{ user }} -->
-      <TweetForm :user="user" />
+      <Head>
+        <Title>Home / Twitter</Title>
+      </Head>
+      <div :class="`${twitterBorderColor} border-b`">
+        <TweetForm :user="user" />
+      </div>
+      <TweetListFeed :tweets="homeTweets" />
     </MainSection>
   </div>
 </template>
